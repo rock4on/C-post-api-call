21c0925f8f5a15e54a85a3a153515e8fcef10c64
rock4on <49546891+rock4on@users.noreply.github.com>
2019-11-05 12:10:53 +0200

    Add files via upload

---
+++ api call/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Xml.Linq;
+using System.Web.Script;
+
+using System.Web.Script.Serialization;
+
+using System.Runtime.Serialization;
+
+namespace api_call
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        WebRequest req;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string site = string.Format("https://api.monkeylearn.com/v3/classifiers/cl_pi3C7JiL/classify/");
+            req = WebRequest.Create(site);
+            req.ContentType = "application/json";
+            req.Method = "POST";
+            req.Headers.Add("Authorization: Token 4ce100a3cd7d8e87a9f9b41f4fd1a168c373b9a5");
+
+
+
+
+
+            string postdata = "{\"data\": [\" " +richTextBox2.Text + "\"]}";
+
+           
+            
+            string txt = "";
+
+            using (var str = new StreamWriter(req.GetRequestStream()))
+            {
+                str.Write(postdata);
+                str.Flush();
+                str.Close();
+                
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+                using (Stream st = resp.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(st);
+                    txt = sr.ReadToEnd();
+                }
+            }
+
+            //       [{"text":" sal","external_id":null,"error":false,"classifications":[{"tag_name":"Neutral","tag_id":60333050,"confidence":0.681}]}]
+
+            //parsare string din json in normal;
+            txt = txt.Substring(txt.IndexOf("\"tag_name\""));
+            txt= txt.Replace(']', ' ');
+            txt= txt.Replace('}',' ');
+
+            string[] auxtext = txt.Split(':', ',');
+
+
+            //   string convert = 
+            richTextBox1.Text = auxtext[1].Trim('\"')+"\n"+"procent: "+ (Double.Parse(auxtext[5].Trim('\"'))*100).ToString() +" %";
+            
+        }
+    }
+}
---
+++ api call/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("api call")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("api call")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ace7870-e010-448e-a4bb-393f95c5ff80")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
